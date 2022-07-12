f8ca6634c4f27f443b60ac5d7a92b69f7d5bf8e6
madwolfcrazy <madwolfcrazy@gmail.com>
2022-07-12 08:55:58 +0800

    更新ant-design-vue 为3.0版本

--- src/utils/http/index.ts
+++ src/utils/http/index.ts
@@ -1,2 +1,16 @@
 import EnclosureHttp from "./core";
+import router from '@/router/index.js'
+import {SessionStorage} from '@/store/localstorage'
+
+EnclosureHttp.initConfig = {
+  beforeResponseCallback: (resp) => {
+    if(resp.data.code == 403) {
+      SessionStorage.remove('info')
+      router.replace('/login')
+    }
+    return resp
+  },
+}
+
+
 export const http = new EnclosureHttp();
